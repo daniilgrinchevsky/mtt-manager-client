def66cd581c6ef499495470bb684912d1a9d613c
Daniil Grinchevskiy <danill.grinchevskiy@1account.net>
2024-03-03 21:43:10 +0700

    V-12 | add page Create -> Task

--- src/pages/Create/Task/index.tsx
+++ src/pages/Create/Task/index.tsx
@@ -57,7 +57,6 @@ const Task: React.FC<props> = ({ show, onHide}) => {
                                         defaultDate: today.getTime(),
                                         time_24hr: true,
                                     }}
-                                    placeholder="Select Time"
                                     className="ltr:rounded-l-none rtl:rounded-r-none form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"
                                 />
                                 <Flatpickr
@@ -68,7 +67,7 @@ const Task: React.FC<props> = ({ show, onHide}) => {
                                         defaultDate: addMinutes(today, 30).getTime(),
                                         time_24hr: true,
                                     }}
-                                    placeholder="Select Time"
+
                                     className="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"
                                 />
                             </div>
